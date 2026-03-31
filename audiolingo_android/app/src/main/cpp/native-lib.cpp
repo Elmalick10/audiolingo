e0ed7fc9404b16ad57b8a9fdea1a6aa4d548d9ff
Elmalick10 <elmalicksenegal@gmail.com>
2026-03-31 14:02:47 +0000

    Premier commit AudioLingo

--- audiolingo_android/app/src/main/cpp/native-lib.cpp
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-#include <string>
-
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_example_audiolingo_MainActivity_stringFromJNI(JNIEnv* env, jobject thiz) {
-    std::string hello = "AudioLingo Core OK 🚀";
-    return env->NewStringUTF(hello.c_str());
-}
